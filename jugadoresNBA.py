32052246fe215c2e3dc5d7853542e1b3ef0e5978
Salazar Zahonero <luis.salazar.zahonero@sap.com>
2023-02-10 14:51:01 +0100

    Initial commit

---
+++ jugadoresNBA.py
@@ -0,0 +1,27 @@
+import requests
+from bs4 import BeautifulSoup
+
+# Hacemos una petición a la página web
+url = 'http://espndeportes.espn.com/basquetbol/nba/jugadores'
+response = requests.get(url)
+
+# Verificamos que la petición haya sido exitosa
+if response.status_code == 200:
+    # Parseamos el contenido de la página web
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    # Buscamos todos los elementos que contengan información sobre los equipos
+    #teams = soup.find_all('td', {'class': 'team'})
+
+    # Obtener todas las etiquetas a
+    links = soup.find_all('a')
+
+    # Filtrar los enlaces por el valor de su atributo href
+    teams = [link for link in links if '/basquetbol/nba/equipo/plantilla/_/nombre/' in link['href']]
+
+    # Imprimimos cada equipo
+    for team in teams:
+        print(team.text.strip())
+else:
+    # En caso de que la petición no haya sido exitosa, mostramos un mensaje de error
+    print('Error al hacer la petición a la página web')
